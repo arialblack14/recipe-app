45480f953ce49f9a0fa216ec09782fb6a07d2899
Alex Tsiras <tsirasalexis@yahoo.gr>
2017-04-02 19:33:11 +0300

    scaffold the needed page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './core/app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateComponent } from './pages/create/create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
